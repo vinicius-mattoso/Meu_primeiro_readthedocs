f1e101e295a4bbed8705ac04c41b3334eb576bc3
Vinicius Mattoso <vinicius.vmrs@gmail.com>
2024-09-03 14:53:58 -0300

    Add new informations to upload

--- conf.py
+++ conf.py
@@ -19,7 +19,7 @@ release = 'alpha_01'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -32,8 +32,41 @@ language = 'pt'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# # Opcional: Se desejar, você pode remover o título do projeto da barra de navegação:
+# Opcional: Se desejar, você pode remover o título do projeto da barra de navegação:
 # html_theme_options = {
 #     "logo_only": True,
 #     "display_version": False,
 # }
+
+# -- Options for LaTeX output ------------------------------------------------
+latex_engine = 'pdflatex'  # Define o engine LaTeX, pdflatex é o mais comum.
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    'papersize': 'a4paper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    'preamble': r'''
+    \usepackage{babel}  % Para suporte ao idioma, como português.
+    \usepackage{amsmath, amssymb, amsfonts}  % Pacotes matemáticos adicionais.
+    \usepackage{graphicx}  % Para inserir gráficos e imagens.
+    \usepackage{enumitem}  % Para listas personalizadas.
+    \setcounter{tocdepth}{2}  % Define a profundidade do sumário.
+    ''',
+
+    # Figure alignment
+    'figure_align': 'H',  # Controla o alinhamento das figuras.
+
+    # Nome personalizado da versão na documentação
+    'releasename': 'Versão',
+}
+
+# Nome do documento principal gerado no LaTeX, geralmente igual ao nome do projeto.
+latex_documents = [
+    ('index', 'BigAnchoringMachineLearningModule.tex',
+     'Big Anchoring Machine Learning Module Documentation',
+     'ICA', 'manual'),
+]
